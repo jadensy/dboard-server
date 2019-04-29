3db3472832fd1d91c14aced75fcf097574dd46bd
jnsy <jadensy99@gmail.com>
2019-04-29 20:05:07 +0800

    update user model

--- models/user.py
+++ models/user.py
@@ -1,6 +1,17 @@
 from models.base_model import BaseModel
 import peewee as pw
 
-
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    username = pw.CharField(unique=True)
+    email = pw.CharField(unique=True)
+    password = pw.CharField()
+
+    def validate(self):
+        # new user validation (check for duplicates)
+        duplicate_username = User.get_or_none(User.username = self.username)
+        duplicate_email = User.get_or_none(User.email == self.email)
+
+        if duplicate_username and not duplicate_username.id == self.id:
+            self.errors.append('Username has been registered. Please try another.')
+        elif duplicate_email and not duplicate_email.id == self.id:
+            self.errors.append('Email has been registered. Please try another.')
