e8c907113f23bd878683bec8cc6f13fac25f6dc3
jnsy <jadensy99@gmail.com>
2019-04-30 17:20:48 +0800

    build client list endpoint

--- board_api/blueprints/users/views.py
+++ board_api/blueprints/users/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, request
 from models.user import User
 from helpers import encode_auth_token, decode_auth_token
 from werkzeug.security import generate_password_hash
