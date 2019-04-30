27e71e6fdce86b8e80623e10bc6f9c785288ea89
jnsy <jadensy99@gmail.com>
2019-04-30 16:34:45 +0800

    build client creation endpoint

--- board_api/__init__.py
+++ board_api/__init__.py
@@ -6,6 +6,11 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 ## API Routes ##
 from board_api.blueprints.users.views import users_api_blueprint
 from board_api.blueprints.sessions.views import sessions_api_blueprint
+from board_api.blueprints.clients.views import clients_api_blueprint
+# from board_api.blueprints.projects.views import projects_api_blueprint
 
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
 app.register_blueprint(sessions_api_blueprint, url_prefix='/api/v1/sessions')
+app.register_blueprint(clients_api_blueprint, url_prefix='/api/v1/clients')
+# app.register_blueprint(projects_api_blueprint, url_prefix='/api/v1/projects')
+
--- models/client.py
+++ models/client.py
@@ -1,4 +1,5 @@
 from models.base_model import BaseModel
+from models.user import User
 import peewee as pw
 
 class Client(BaseModel):
