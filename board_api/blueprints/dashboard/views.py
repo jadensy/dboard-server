c5bb742424ae5451e03c50a47311b15d156b30ec
jnsy <jadensy99@gmail.com>
2019-05-05 06:24:48 +0800

    build endpoint to return all dashboard data

--- board_api/blueprints/dashboard/views.py
+++ board_api/blueprints/dashboard/views.py
@@ -3,6 +3,7 @@ from models.user import User
 from models.client import Client
 from models.project import Project
 from helpers import encode_auth_token, decode_auth_token
+from peewee import fn
 
 dashboard_api_blueprint = Blueprint('dashboard_api',
                                   __name__,
@@ -23,27 +24,45 @@ def index():
     if user:
         project_count = Project.select().count()
         client_count = Client.select().count()
+        currency_types = {}
+        project_types = {}
         currency_revenue = {}
         project_revenue = {}
 
         currencies = Project.select(Project.currency.distinct())
-        project_types = Project.select(Project.project_type.distinct())
+        pr_type_count = Project.select(Project.project_type.distinct())
         countries = Client.select(Client.country.distinct()).count()
 
         for c in currencies:
             count = Project.select().where(Project.currency == c.currency).count()
             cr = {c.currency: count}
-            currency_revenue.update(cr)
+            currency_types.update(cr)
 
-        for t in project_types:
+            revenue = Project.select(Project.total).where(Project.currency == c.currency)
+            crt = []
+            for r in revenue:
+                crt.append(r.total)
+                c_total = { c.currency: str(sum(crt)) }
+                currency_revenue.update(c_total)
+
+        for t in pr_type_count:
             count = Project.select().where(Project.project_type == t.project_type).count()
             pt = {t.project_type: count}
-            project_revenue.update(pt)
+            project_types.update(pt)
+
+            revenue = Project.select(Project.total).where(Project.project_type == t.project_type)
+            prt = []
+            for r in revenue:
+                prt.append(r.total)
+                t_total = { t.project_type: str(sum(prt)) }
+                project_revenue.update(t_total)
 
         db_basic_data = {
             "project_count": project_count,
             "client_count": client_count,
             "countries": countries,
+            "currency_types": currency_types,
+            "project_types": project_types,
             "currency_revenue": currency_revenue,
             "project_revenue": project_revenue,
         }
